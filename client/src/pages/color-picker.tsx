d5ef92ab12a58bb502d0da9d916775de9fe4978d
yukiokun02 <yukio.takahashi11@gmail.com>
2025-05-29 03:37:12 +0530

    Update color-picker.tsx

--- client/src/pages/color-picker.tsx
+++ client/src/pages/color-picker.tsx
@@ -333,7 +333,8 @@ export default function ColorPicker() {
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
         <div className="bg-gray-800/30 rounded-lg p-3 text-center text-gray-400 border border-gray-600/30 mb-6">
           <div className="h-16 sm:h-20 lg:h-24 flex items-center justify-center text-xs sm:text-sm">
-            Top Ad Banner (728x90 / 320x50)
+            <AdsterraAdMobile />
+            <AdsterraAdDesktop />
           </div>
         </div>
       </div>
@@ -534,7 +535,8 @@ export default function ColorPicker() {
             <div className="my-8">
               <div className="bg-gray-800/30 rounded-lg p-4 text-center text-gray-400 border border-gray-600/30">
                 <div className="h-20 sm:h-24 lg:h-28 flex items-center justify-center text-sm">
-                  Inline Ad Area (728x90 / 320x100)
+                  <AdsterraAdMobile />
+                  <AdsterraAdDesktop />
                 </div>
               </div>
             </div>
